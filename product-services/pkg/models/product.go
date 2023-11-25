bd616cf651560c8c172951628e480dd32aedacb8
abhinandpn <abhinandpn4067@gmail.com>
2023-11-25 11:55:12 +0530

    updating

--- product-services/pkg/models/product.go
+++ product-services/pkg/models/product.go
@@ -1,17 +1,10 @@
 package models
 
 type Product struct {
-<<<<<<< HEAD
-	Id        uint   `json:"id" gorm:"primaryKey;unique;not null"`
-	UserName  string `json:"username" gorm:"not null"`
-	Email     string `json:"email" gorm:"not null;unique"`
-	Password  string `json:"password" gorm:"not null"`
-	IsBlocked bool
-=======
-	Id          uint   `json:"id" gorm:"primaryKey;unique;not null"`
-	ProductName string `json:"productname" gorm:"not null"`
-	Discription string `json:"discription" gorm:"not null;unique"`
-	Image       string `json:"image" gorm:"not null"`
-	Stock       uint   `json:"stock" gorm:"not null"`
->>>>>>> user
+	Id          uint
+	ProductName string
+	Discription string
+	Price       float64
+	Quantity    int
+	Category    string
 }
