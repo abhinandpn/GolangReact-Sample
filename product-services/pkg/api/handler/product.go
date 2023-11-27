13748612340513173d751d69a3b9a4767535e27f
abhinandpn <abhinandpn4067@gmail.com>
2023-11-27 21:04:27 +0530

    function extablished

--- product-services/pkg/api/handler/product.go
+++ product-services/pkg/api/handler/product.go
@@ -24,21 +24,21 @@ func NewProductHandler(productUsecase services.ProductUseCase) handlerinterfaces
 // @Success 200 {object} res.Response{} "successfully product added"
 // @Failure 400 {object} res.Response{} "invalid input"
 func (P *ProductHandler) AddProduct(ctx *gin.Context) {
-
+	panic("unimplemented AddProduct --> Handler")
 }
 
 func (P *ProductHandler) UpdateProduct(ctx *gin.Context) {
-
+	panic("unimplemented UpdateProduct --> Handler")
 }
 
 func (P *ProductHandler) DeteleProduct(ctx *gin.Context) {
-
+	panic("unimplemented DeteleProduct --> Handler")
 }
 
 func (P *ProductHandler) ListFullProducts(ctx *gin.Context) {
-
+	panic("unimplemented ListFullProducts --> Handler")
 }
 
 func (P *ProductHandler) GetProductById(ctx *gin.Context) {
-
+	panic("unimplemented GetProductById --> Handler")
 }
