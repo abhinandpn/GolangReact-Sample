83d2ad94cc679d11863700ccc44aae757e999476
abhinandpn <abhinandpn4067@gmail.com>
2023-11-27 21:05:56 +0530

    admin services updated

--- admin-services/pkg/usecase/interfaces/admininterfaces.go
+++ admin-services/pkg/usecase/interfaces/admininterfaces.go
@@ -2,12 +2,10 @@ package interfaces
 
 import (
 	"context"
-
-	"github.com/abhinandpn/MicroServices-GoLang/admin-services/pkg/models"
 )
 
 type AdminUseCase interface {
-	AdminLogin(ctx context.Context, admin models.Admin) (models.Admin, error)
-	// AdminHome(ctx context.Context)
-	// AdminLogout()
+	AdminLogin(ctx context.Context)
+	AdminHome(ctx context.Context)
+	AdminLogout(ctx context.Context)
 }
